8a8cabb46d141fa6f73be2459c1d1843cf397577
Stephen Sayage <stephensayage@gmail.com>
2020-10-07 12:07:17 -0400

    bucket list component created with some starting data

---
+++ client/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <ul>
+    <bucket-list-item
+      v-for="item in storedListItems"
+      :key="item.id"
+      :title="item.title"
+      :desc="item.desc"
+      :link="item.link"
+    ></bucket-list-item>
+  </ul>
+</template>
+
+<script>
+import BucketListItem from './components/BucketList/BucketListItem.vue';
+
+export default {
+  components: {
+    BucketListItem
+  },
+  data() {
+    return {
+      storedListItems: [
+        {
+          id: '1',
+          title: 'Offical Guide to Buckit',
+          desc:
+            'Please create as many bucket list items as you want! Links can be included to resources',
+          link: 'https://personalexcellence.co/blog/bucket-list/'
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Nunito&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Nunito', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ client/src/components/BucketList/BucketListItem.vue
@@ -0,0 +1,50 @@
+<template>
+  <li>
+    <div>
+      <header>
+        <h3>{{ title }}</h3>
+        <button>Delete</button>
+      </header>
+    </div>
+    <p>{{ desc }}</p>
+    <nav>
+      <a :href="link" target="_blank">Link</a>
+    </nav>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ['title', 'desc', 'link']
+};
+</script>
+
+<style scoped>
+li {
+  margin: auto;
+  max-width: 40rem;
+}
+
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+h3 {
+  font-size: 26px;
+  margin: 10px 0;
+}
+
+p {
+  margin: 10px 0;
+}
+
+a {
+  text-decoration: none;
+  color: rgb(0, 163, 217);
+}
+a:hover {
+  color: rgb(0, 98, 130);
+}
+</style>
